e30956322813dd29ad2309f588a253be66065347
“devtommie” <devtommie114@gmail.com>
2024-06-13 23:31:51 -0400

    sidebar implemented

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Sidebar from '@/components/Sidebar'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const loggedIn = {firstName: 'Devtommie', lastName: 'Martinez'}
+
+
+    return (
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn} />
+            {children}
+      </main>
+  );
+}
