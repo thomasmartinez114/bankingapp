d74dc4d2c633a55f9a332ae3d0b2dc3621f4624b
“devtommie” <devtommie114@gmail.com>
2024-06-14 02:13:16 -0400

    added cn Sheet for Open button on navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,27 @@
-import Sidebar from '@/components/Sidebar'
+import Sidebar from '@/components/Sidebar';
+import MobileNav from '@/components/MobileNav';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Devtommie', lastName: 'Martinez' };
 
-  const loggedIn = {firstName: 'Devtommie', lastName: 'Martinez'}
+  return (
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedIn} />
 
-
-    return (
-        <main className="flex h-screen w-full font-inter">
-            <Sidebar user={loggedIn} />
-            {children}
-      </main>
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image src='/icons/logo.svg' width={30} height={30} alt='logo' />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
