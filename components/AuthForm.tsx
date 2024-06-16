6cc5ed8576dc11e4acd49782079385beb3ac0033
“devtommie” <devtommie114@gmail.com>
2024-06-16 00:00:00 -0400

    added user statee

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const AuthForm = ({ type }: { type: string }) => {
-  return <div>{type}</div>;
+  const [user, setUser] = useState(null);
+
+  return (
+    <section className='auth-form'>
+      <header className='flex flex-col gap-5 md:gap-8'>
+        <Link href='/' className='mb-12 cursor-pointer flex items-center gap-1'>
+          <Image
+            src='/icons/logo.svg'
+            width={34}
+            height={34}
+            alt='Horizon Logo'
+          />
+          <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>
+            Horizon
+          </h1>
+        </Link>
+        <div className='flex flex-col gap-1 md:gap-3'>
+          <h1>
+            {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+          </h1>
+        </div>
+      </header>
+    </section>
+  );
 };
 
 export default AuthForm;
