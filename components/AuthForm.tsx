d71bc13c178bc5f8c701b47adc5d8eb9fc263900
“devtommie” <devtommie114@gmail.com>
2024-06-17 01:32:32 -0400

    push

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,8 +22,9 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
-const AuthForm = ({ type }: { type: string; }) => {
+const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -47,22 +48,19 @@ const AuthForm = ({ type }: { type: string; }) => {
       // Sign up with Appwrite & create plaid token
 
       if (type === 'sign-up') {
-        // const newUser = await signUp(data);
+        const newUser = await signUp(data);
 
-        // setUser(newUser);
-
+        setUser(newUser);
       }
 
       if (type === 'sign-in') {
-        // const response = await SignIn({
-        //   email: data.email,
-        //   password: data.password
-        // })
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
 
-        // if(response) router.push('/')
-
+        if (response) router.push('/');
       }
-
     } catch (error) {
       console.log(error);
     } finally {
