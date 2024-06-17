b4a45d2883933f7b047dcd9c1cbca15badbed4f7
“devtommie” <devtommie114@gmail.com>
2024-06-16 23:41:10 -0400

    Added form Button and Loading spinner

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,9 +20,11 @@ import {
 import { Input } from '@/components/ui/input';
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof authFormSchema>>({
@@ -37,7 +39,9 @@ const AuthForm = ({ type }: { type: string }) => {
   function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true);
     console.log(values);
+    setIsLoading(false);
   }
 
   return (
@@ -84,7 +88,18 @@ const AuthForm = ({ type }: { type: string }) => {
                 label='Password'
                 placeholder='Enter your password'
               />
-              <Button type='submit'>Submit</Button>
+              <Button type='submit' disabled={isLoading} className='form-btn'>
+                {isLoading ? (
+                  <>
+                    <Loader2 size={20} className='animate-spin' /> &nbsp;
+                    Loading...
+                  </>
+                ) : type === 'sign-in' ? (
+                  'Sign In'
+                ) : (
+                  'Sign Up'
+                )}
+              </Button>
             </form>
           </Form>
         </>
