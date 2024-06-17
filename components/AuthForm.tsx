bd538d5fa60132418f480bcf73273712acdd4bd0
“devtommie” <devtommie114@gmail.com>
2024-06-17 02:20:50 -0400

    creating acct functional

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,12 +22,13 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { signIn, signUp } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  
 
   const formSchema = authFormSchema(type);
 
@@ -54,12 +55,12 @@ const AuthForm = ({ type }: { type: string }) => {
       }
 
       if (type === 'sign-in') {
-        const response = await signIn({
-          email: data.email,
-          password: data.password,
-        });
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // });
 
-        if (response) router.push('/');
+        // if (response) router.push('/');
       }
     } catch (error) {
       console.log(error);
