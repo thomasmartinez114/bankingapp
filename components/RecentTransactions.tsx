8b06eb6ccdc9288b5c3ec7e6057f1af3800f2d7d
“devtommie” <devtommie114@gmail.com>
2024-06-23 21:48:57 -0400

    view all button

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 const RecentTransactions = ({
@@ -7,7 +8,12 @@ const RecentTransactions = ({
     page = 1
 }: RecentTransactionsProps) => {
   return (
-    <div>RecentTransactions</div>
+      <section className='recent-transactions'>
+          <header className="flex items-center justify-between">
+              <h2 className="recent-transactions-label">Recent Transactions</h2>
+              <Link href={`/transactions-history/?id=${appwriteItemId}`} className='view-all-btn'>View All</Link>
+          </header>
+    </section>
   )
 }
 
